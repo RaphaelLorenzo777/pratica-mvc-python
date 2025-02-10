4704851e4daaa5472eee634d12192389f5e18cc8
Raphael Lorenzo <raphaelvarjao0@gmail.com>
2025-02-10 16:15:44 -0300

    UP

--- model/database.py
+++ model/database.py
@@ -6,4 +6,17 @@ from os import getenv
 class Database:
     def __init__(self):
         load_dotenv()
-        self.host =
+        self.host = getenv( 'DB_HOST' )
+        self.username = getenv( 'DB_USER' )
+        self.password = getenv( 'DB_PSWD' )
+        self.database = getenv( 'DB_NAME' )
+        self.connection = None
+        self.cursor = None
+
+    def conectar(self):
+        """Estabelece uma conexão com o banco de dados."""
+        try:
+            self.connectionn = mysql.connector.connect(
+                host = self.host,
+                database = self.database, 
+            )
