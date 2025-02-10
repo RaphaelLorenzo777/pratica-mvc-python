07be9dc560823067f86b554ee6194c23a3cea387
Raphael Lorenzo <raphaelvarjao0@gmail.com>
2025-02-10 13:52:14 -0300

    update

--- model/database.py
+++ model/database.py
@@ -2,3 +2,8 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 from os import getenv
+
+class Database:
+    def __init__(self):
+        load_dotenv()
+        self.host =
